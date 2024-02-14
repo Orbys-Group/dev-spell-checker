46d4b3ba83bccdfb73cdfe0b7f4502b45d09b220
taylor mosquera castro <thaylor0184@hotmail.com>
2024-02-14 10:48:02 -0500

    Añadir funcionalidad de borrado y navegación en el archivo main.js

--- media/main.js
+++ media/main.js
@@ -4,12 +4,45 @@
 // It cannot access the main VS Code APIs directly.
 (function () {
     const vscode = acquireVsCodeApi();
-    const botones = document.querySelectorAll('.delete').forEach((button) => {
+    document.querySelectorAll('.delete').forEach((button) => {
         button.addEventListener('click', () => {
             vscode.postMessage({
                 type: 'delete',
                 value: button.getAttribute('data-id')
             });
         });
+    });
+
+    tippy('.delete', {
+        content: 'Borrar',
+    });
+    document.querySelectorAll('.goto').forEach((button) => {
+        button.addEventListener('click', () => {
+            vscode.postMessage({
+                type: 'goto',
+                value: button.getAttribute('data-id')
+            });
+        });
+    });
+    document.querySelectorAll('.back').forEach((button) => {
+        button.addEventListener('click', () => {
+            vscode.postMessage({
+                type: 'back',
+                value: button.getAttribute('data-id')
+            });
+        });
+    });
+    tippy('.goto', {
+        content: 'Ver archivo',
+    });
+    tippy('.back', {
+        content: 'Restablecer',
+    });
+    const loader = document.querySelector('.loader');
+    loader.style.display = 'none';
+    window.addEventListener("message", (event) => {
+        if (event.data.type === 'loader') {
+            loader.style.display = event.data.value === 'show' ? 'flex' : 'none';
+        }
     });
 })();
